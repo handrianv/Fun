924960fce9484f4e97b900d8836ac793df850d0f
Handrian Vittorio <handrian.vittorio@traveloka.com>
2019-06-30 15:36:01 +0700

    2001

---
+++ 2001 - Mathematicians and Berries.cpp
@@ -0,0 +1,29 @@
+/*
+*/
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define LSOne(S) (S & (-S))
+#define EPS 1e-9
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pi;
+typedef tree<string, null_type, less<string>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    //cin.tie(0); // FOR QUERY PROBLEM DON'T FORGET TO UNCOMMENT THIS
+    int a, b, a2, b2, a3, b3;
+    cin >> a >> b >> a2 >> b2 >> a3 >> b3;
+    cout << (a-a3) << " " << (b-b2) << "\n";
+    return 0;
+}   
